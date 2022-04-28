eb6db84dc2cca77b2146510056dca05d88187d4e
nili-vjd <armita.vajed@gmail.com>
2022-04-28 05:14:22 +0430

    Added Dictionary JS file and added some css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <main>
+            <Dictionary />
+          </main>
+          <footer className="App-footer">
+            {" "}
+            <small>Coded by Armita</small>
+          </footer>
+        </header>
+      </div>
     </div>
   );
 }
