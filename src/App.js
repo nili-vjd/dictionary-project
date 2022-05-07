1b96a065e4af4346bf9f216c2855d5780f580977
nili-vjd <armita.vajed@gmail.com>
2022-05-08 04:23:11 +0430

    Added GitHub link

--- src/App.js
+++ src/App.js
@@ -9,11 +9,20 @@ export default function App() {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <main>
-            <Dictionary defaultKeyword="sunset" />
+            <Dictionary defaultKeyword="Diamond" />
           </main>
           <footer className="App-footer">
             {" "}
-            <small>Coded by Armita</small>
+            <small>
+              Coded by Armita and is{" "}
+              <a
+                href="https://github.com/nili-vjd/dictionary-project"
+                target="_blank"
+                rel="noreferrer"
+              >
+                open-sourced on GitHub
+              </a>
+            </small>
           </footer>
         </header>
       </div>
