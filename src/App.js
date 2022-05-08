92ff8b44c855f6617b5ab03be8527878e9e0c5a0
nili-vjd <armita.vajed@gmail.com>
2022-05-08 04:31:21 +0430

    Added some links

--- src/App.js
+++ src/App.js
@@ -9,18 +9,34 @@ export default function App() {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <main>
-            <Dictionary defaultKeyword="Diamond" />
+            <Dictionary defaultKeyword="diamond" />
           </main>
           <footer className="App-footer">
             {" "}
             <small>
-              Coded by Armita and is{" "}
+              Coded by{" "}
+              <a
+                href="https://www.linkedin.com/in/armita-vajed/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Armita{" "}
+              </a>
+              and is{" "}
               <a
                 href="https://github.com/nili-vjd/dictionary-project"
                 target="_blank"
                 rel="noreferrer"
               >
                 open-sourced on GitHub
+              </a>{" "}
+              and{" "}
+              <a
+                href="https://starlit-paletas-f5f1e6.netlify.app/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                hosted on Netlify
               </a>
             </small>
           </footer>
